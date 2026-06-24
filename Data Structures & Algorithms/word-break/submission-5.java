7d045bd87b21aecb7e1988b437ad9dd0a71ab16d
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-06-23 23:43:46 -0700

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+    HashMap<String,Boolean> mp;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        mp = new HashMap<>();
+
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[][] t = new boolean[n+1][n+1];
+
+        return word(s,words,t);
+    }
+
+    public boolean word(String s, Set<String> st, boolean[][] t){
+        int n = s.length();
+
+        if(st.contains(s)) return true;
+        if(mp.containsKey(s)) return mp.get(s);
+
+        for(int k=1; k<n; k++){
+            boolean firstStr = word(s.substring(0,k),st,t); 
+
+            if(!firstStr) continue;
+            boolean secStr = word(s.substring(k,n),st,t);
+
+            if((firstStr) && (secStr)) return t[n][n] = true;
+        }
+
+        mp.put(s,false);
+        return false;
+    }
+}
