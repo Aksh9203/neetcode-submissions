cf69286babe8de26ad0737e8a3283730d481a385
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-07-01 21:01:08 -0700

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.java
@@ -0,0 +1,55 @@
+// class Solution {
+//     public List<Integer> partitionLabels(String s) {
+//         int n = s.length();
+
+//         int[] lastOccur = new int[26];
+
+//         for(int i=0; i<n; i++){ 
+//             lastOccur[s.charAt(i) - 'a'] = i;
+//         }
+
+//         int i = 0;
+//         int start = 0;
+//         int end = 0;
+
+//         ArrayList<Integer> ls = new ArrayList<>();
+
+//         while(i<n){
+//             end = Math.max(end,lastOccur[s.charAt(i) - 'a']);
+
+//             if(i == end){
+//                 ls.add(end - start + 1);
+//                 start = i + 1;
+//             }
+//             i++;
+//         }
+//         return ls;
+//     }
+// }
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+
+        Map<Character,Integer> mp = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            mp.put(s.charAt(i),i);
+        }
+
+        int start = 0;
+        int end = 0;
+
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            end = Math.max(end,mp.get(s.charAt(i)));
+
+            if(i == end){
+                ls.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return ls;
+    }
+}
