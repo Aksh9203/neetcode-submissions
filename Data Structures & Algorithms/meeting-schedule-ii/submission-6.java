3af620e949286cd76cf6d3105d9699499d86292b
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-07-25 22:52:36 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.java
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+
+        if(n <= 1) return n;
+
+        Collections.sort(intervals,(a,b) -> a.start - b.start);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(intervals.get(0).end);
+
+        for(int i = 1; i<n; i++){
+            if(intervals.get(i).start >= pq.peek()) pq.poll();
+            pq.add(intervals.get(i).end);
+        }
+
+        return pq.size();
+    }
+}
