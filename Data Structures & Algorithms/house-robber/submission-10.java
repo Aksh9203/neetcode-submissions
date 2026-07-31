7a588824f2ce5f8f056765acbfdc1d24b1199817
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-07-31 09:15:07 -0700

    Add: house-robber - submission-10

---
+++ Data Structures & Algorithms/house-robber/submission-10.java
@@ -0,0 +1,44 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+
+//         if(n==1) return nums[0];
+//         if(n==2) return Math.max(nums[1],nums[0]);
+
+//         int[] t = new int[nums.length + 1];
+//         Arrays.fill(t,-1);
+
+//         return Math.max(pickHouse(nums,0,t), pickHouse(nums,1,t));
+//     }
+
+//     public int pickHouse(int[] nums, int idx, int[] t){
+//         if(idx >= nums.length) return 0;
+
+//         if(t[idx] != -1) return t[idx];
+
+//         int totalCost  = nums[idx] + Math.max(pickHouse(nums,idx + 2, t), pickHouse(nums,idx + 3, t));
+
+//         return t[idx] = totalCost;
+//     }
+// }
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[1],nums[0]);
+
+        int[] t = new int[nums.length + 1];
+
+        t[0] = nums[0];
+        t[1] = nums[1];
+        t[2] = nums[0] + nums[2];
+
+        for(int i=3; i<n; i++){
+            t[i] = nums[i] + Math.max(t[i-2],t[i-3]);
+        }
+
+        return Math.max(t[n-1],t[n-2]);
+    }
+}
