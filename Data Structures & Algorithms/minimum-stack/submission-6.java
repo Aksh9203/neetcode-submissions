1cd90669718415a5f625fbc372f51f92311d15df
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-03 08:14:49 -0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,40 @@
+class MinStack {
+
+    Stack<int[]> st;
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(!st.isEmpty()){
+            int min = st.peek()[1];
+            if(val < min){
+                st.push(new int[]{val,val});
+            }
+            else{
+                st.push(new int[]{val,min});
+            }
+        }
+        else{
+            st.push(new int[]{val,val});
+        }
+    }
+    
+    public void pop() {
+        st.pop();
+    }
+    
+    public int top() {
+        if(!st.isEmpty()){
+            int top = st.peek()[0];
+            return top;
+        }
+
+        return -1;
+    }
+    
+    public int getMin() {
+        int top = st.peek()[1];
+        return top;
+    }
+}
