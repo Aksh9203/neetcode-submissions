540dd10b0c5f9677c0a440f840b598b69c3d28ca
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-06 09:42:19 -0700

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+
+        subset(nums,ans,current,0);
+        return ans;
+    }
+
+    public void subset(int[] nums, List<List<Integer>> ans, List<Integer> current, int idx){
+        ans.add(new ArrayList<>(current));
+
+        for(int i=idx; i<nums.length; i++){
+            if(i > idx && nums[i] == nums[i-1]) continue;
+            current.add(nums[i]);
+            subset(nums,ans,current,i+1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
