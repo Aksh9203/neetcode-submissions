291bfd3bfbd1ecfe45e4157910ff24ce4ef93791
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-07 11:44:05 -0700

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.java
@@ -0,0 +1,51 @@
+class DSU{
+    int[] parent;
+    int[] size;
+
+    public DSU(int n){
+        parent = new int[n+1];
+        size = new int[n+1];
+
+        for(int i=0; i<=n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = find(parent[node]);
+    }
+
+    public void union(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu == pv) return;
+        else if(size[pu] > size[pv]){
+            size[pu] += size[pv];
+            parent[pv] = pu;  
+        }
+        else{
+            size[pv] += size[pu];
+            parent[pu] = pv;
+        }
+    }
+}
+
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        DSU dsu = new DSU(edges.length);
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(dsu.find(u) == dsu.find(v)){
+                return new int[]{u,v};
+            }
+            dsu.union(u,v);
+        }
+        return new int[]{};
+    }
+}
