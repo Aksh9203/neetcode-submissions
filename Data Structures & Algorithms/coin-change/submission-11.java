dabfc0d56d5cb32dd434ca345374728ffebfaaa5
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-09 09:08:05 -0700

    Add: coin-change - submission-11

---
+++ Data Structures & Algorithms/coin-change/submission-11.java
@@ -0,0 +1,61 @@
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int n = coins.length;
+
+//         if(n == 1){
+//             if(amount % coins[0] != 0) return -1;
+//             else return amount/coins[0];
+//         }
+
+//         int[][] t = new int[n + 1][amount + 1];
+//         for(int[] t1 : t){
+//             Arrays.fill(t1,-1);
+//         }
+
+//         int result = coinNum(coins,amount,n,t);
+
+//         return (result >= Integer.MAX_VALUE - 1) ? -1 : result;
+//     }
+
+//     public int coinNum(int[] coins, int amount, int n, int[][] t){
+//         if(n == 0 && amount == 0) return 0;
+//         if(n == 0 && amount > 0) return Integer.MAX_VALUE - 1;
+
+//         if(t[n][amount] != -1) return t[n][amount];
+
+//         if(coins[n-1] <= amount){
+//             return t[n][amount] = Math.min(1 + coinNum(coins,amount - coins[n-1], n, t) , coinNum(coins, amount, n - 1, t));
+//         }
+
+//         return t[n][amount] = coinNum(coins, amount, n - 1, t);
+//     }
+// }
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+
+        if(n == 1){
+            if(amount % coins[0] != 0) return -1;
+            else return amount/coins[0];
+        }
+
+        int[][] t = new int[n + 1][amount + 1];
+
+        for(int j=0; j<amount + 1; j++){
+            t[0][j] = Integer.MAX_VALUE - 1;
+        }
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<amount+1; j++){
+                if(coins[i - 1] <= j){
+                    t[i][j] = Math.min(1 + t[i][j - coins[i - 1]] , t[i-1][j]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][amount] >= Integer.MAX_VALUE - 1 ? -1 : t[n][amount];
+    }
+}
