397b24b773782287d7077499e7b84696ce037bcc
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-11 15:47:46 -0700

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        double[][] fleet = new double[n][2];
+
+        for(int i=0; i<n; i++){
+            fleet[i][0] = position[i];
+            fleet[i][1] = (double)(target - position[i])/speed[i];
+        }
+
+        Arrays.sort(fleet, (a,b) -> Double.compare(b[0],a[0]));
+
+        double currTime = 0;
+        int cnt = 0;
+
+        for(int i=0; i<n; i++){
+            double time = fleet[i][1];
+
+            if(time > currTime){
+                cnt++;
+                currTime = time;
+            }
+        }
+        return cnt;
+    }
+}
