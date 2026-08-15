51909b2d90dc787e35bba58c2f4ae381d87b6414
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-15 08:04:40 -0700

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        if(digits.length() == 0) return ans;
+
+        comb(digits,ans,sb,0);
+        return ans;
+    }
+
+    public void comb(String digits, List<String> ans, StringBuilder sb, int idx){
+        if(idx == digits.length()){
+            ans.add(sb.toString());
+            return;
+        }
+
+        char digit = digits.charAt(idx);
+        String letter = mapping[digit - '0'];
+
+        for(int i=0; i<letter.length(); i++){
+            sb.append(letter.charAt(i));
+            comb(digits,ans,sb,idx+1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
