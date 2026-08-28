cf25d6f4f8794e94dc61b7ee58d5e6c61b19b98c
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-08-27 18:28:10 -0700

    Add: matchsticks-to-square - submission-5

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-5.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        Arrays.sort(matchsticks);
+
+        int n = matchsticks.length;
+
+        int total = 0;
+        for(int stick : matchsticks){
+            total += stick;
+        }
+
+        // To make sure each side of equal length is possible.
+        if(total % 4 != 0) return false;
+
+        // total/4 is length of each side.
+        return dfs(matchsticks, n, total/4, total/4, total/4, total/4);
+    }
+
+    private boolean dfs(int[] nums, int n, int side1, int side2, int side3, int side4){
+
+        //If value of any side become -ve then return false.
+        if(side1 < 0 || side2 < 0 || side3 < 0 || side4 < 0) return false;
+
+        //After using all matchsticks and still any of the side is not equal to length then return false else true.
+        if(n == 0) {
+            return side1 == 0 && side2 == 0 && side3 == 0 && side4 == 0;
+        }
+
+        //If size of matchstick is greater than all side return false.
+        if(nums[n-1] > side1 && nums[n-1] > side2 && nums[n-1] > side3 && nums[n-1] > side4) return false;
+
+        //Optimized branching with Duplicate pruning
+
+        //Always check side 1 first.
+        if(dfs(nums, n - 1, side1 - nums[n-1], side2, side3, side4)) return true;
+
+        //Try side 2 only if its capacity is unique (not equal to side1)
+        if(side2 != side1){
+            if(dfs(nums, n - 1, side1, side2 - nums[n-1], side3, side4)) return true;
+        }
+
+        //Try side 3 only if its capacity is unique (not equal to side1 or side2)
+        if(side3 != side2 && side3 != side1){
+            if(dfs(nums, n - 1, side1, side2, side3 - nums[n-1], side4)) return true;
+        }
+
+        //Try side 4 only if its capacity is unique (not equal to side1, side2, or side3)
+        if(side4 != side3 && side4 != side2 && side4 != side1){
+            if(dfs(nums, n - 1, side1, side2, side3, side4 - nums[n-1])) return true;
+        }
+
+        return false; 
+    }
+}
