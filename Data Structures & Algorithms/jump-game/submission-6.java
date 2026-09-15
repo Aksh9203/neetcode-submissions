d0b4eda3467287f9cc47670c0523d20757622560
Aksh9203 <118156173+Aksh9203@users.noreply.github.com>
2026-09-14 20:29:47 -0700

    Add: jump-game - submission-6

---
+++ Data Structures & Algorithms/jump-game/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        int maxJump = 0;
+
+        for(int i=0; i<n; i++){
+            if(maxJump < i) return false;
+            maxJump = Math.max(maxJump, i + nums[i]);
+        }
+        return true;
+    }
+}
